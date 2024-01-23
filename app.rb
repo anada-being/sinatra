8fe708fd6ccf2ebf880c88b22de850246118b8ea
anada <anada@beingcorp.co.jp>
2024-01-23 15:04:32 +0900

    定数の引数を無しで修正

--- app.rb
+++ app.rb
@@ -6,12 +6,14 @@ require 'json'
 enable :method_override
 FILE_PATH = 'memos.json'
 
-def get_memos(file_path)
-  File.open(file_path) { |f| JSON.parse(f.read) }
+def get_memos
+  File.open(FILE_PATH) { |f| JSON.parse(f.read) }
 end
 
-def set_memos(file_path, memos)
-  File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
+def set_memos(memos)
+  File.open(FILE_PATH,'w') { |f| JSON.dump(memos, f) }
+end
+
 end
 
 get '/' do
@@ -19,7 +21,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = get_memos(FILE_PATH)
+  @memos = get_memos
   erb :top
 end
 
@@ -28,7 +30,7 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   @title = memos[params[:id]]['title']
   @memo = memos[params[:id]]['memo']
   erb :show
@@ -38,16 +40,16 @@ post '/memos' do
   title = params[:title]
   memo = params[:memo]
 
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   id = (memos.keys.map(&:to_i).max + 1).to_s
   memos[id] = { 'title' => title, 'memo' => memo }
-  set_memos(FILE_PATH, memos)
+  set_memos(memos)
 
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   @title = memos[params[:id]]['title']
   @memo = memos[params[:id]]['memo']
   erb :edit
@@ -57,17 +59,17 @@ patch '/memos/:id' do
   title = params[:title]
   memo = params[:memo]
 
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   memos[params[:id]] = { 'title' => title, 'memo' => memo }
-  set_memos(FILE_PATH, memos)
+  set_memos(memos)
 
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   memos.delete(params[:id])
-  set_memos(FILE_PATH, memos)
+  set_memos(memos)
 
   redirect '/memos'
 end
