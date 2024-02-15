389f4665376b9a7d756ecc20ee30ee4324607174
anada <anada@beingcorp.co.jp>
2024-02-15 13:21:20 +0900

    クラス変数をクラスインスタンス変数に（rubocopの警告により、変更した）ハッシュの書き方を変更

--- app.rb
+++ app.rb
@@ -6,38 +6,39 @@ require 'erb'
 
 # controller
 class App < Sinatra::Application
+  @conn = PG.connect(dbname: 'db_memos')
+
   helpers do
     include ERB::Util
   end
 
   configure do
-    @@conn = PG.connect(dbname: 'db_memos')
-    @@conn.exec('create table IF NOT EXISTS memos (id serial, title TEXT, memo TEXT) ')
+    @conn.exec('create table IF NOT EXISTS memos (id serial, title TEXT, memo TEXT)')
   end
 
-  def to_utf8(text)
+  def self.to_utf8(text)
     text.force_encoding('utf-8')
   end
 
-  def read_memos
-    @@conn.exec('SELECT * FROM memos ORDER BY id').map { |r| { :id => r['id'], :title => to_utf8(r['title']), :memo => to_utf8(r['memo']) } }
+  def self.read_memos
+    @conn.exec('SELECT * FROM memos ORDER BY id').map { |r| { id: r['id'], title: App.to_utf8(r['title']), memo: App.to_utf8(r['memo']) } }
   end
 
-  def read_memo(id)
-    result = @@conn.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
-    { :id => result[0]['id'], :title => to_utf8(result[0]['title']), :memo => to_utf8(result[0]['memo']) } 
+  def self.read_memo(id)
+    result = @conn.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
+    { id: result[0]['id'], title: App.to_utf8(result[0]['title']), memo: App.to_utf8(result[0]['memo']) }
   end
 
-  def create_memo(title, memo)
-    @@conn.exec_params('INSERT INTO memos (title, memo) VALUES ($1, $2);', [title, memo])
+  def self.create_memo(title, memo)
+    @conn.exec_params('INSERT INTO memos (title, memo) VALUES ($1, $2);', [title, memo])
   end
 
-  def patch_memo(id, title, memo)
-    @@conn.exec_params('UPDATE memos SET title = $1, memo = $2 WHERE id = $3;', [title, memo, id])
+  def self.patch_memo(id, title, memo)
+    @conn.exec_params('UPDATE memos SET title = $1, memo = $2 WHERE id = $3;', [title, memo, id])
   end
 
-  def delete_memo(id)
-    @@conn.exec_params('DELETE from memos where id = $1;', [id])
+  def self.delete_memo(id)
+    @conn.exec_params('DELETE from memos where id = $1;', [id])
   end
 
   get '/' do
@@ -45,7 +46,7 @@ class App < Sinatra::Application
   end
 
   get '/memos' do
-    @memos = read_memos
+    @memos = App.read_memos
     erb :top
   end
 
@@ -56,20 +57,20 @@ class App < Sinatra::Application
   post '/memos' do
     title = params[:title]
     memo = params[:memo]
-    create_memo(title, memo)
+    App.create_memo(title, memo)
 
     redirect '/memos'
   end
 
   get '/memos/:id' do
-    memo = read_memo(params[:id])
+    memo = App.read_memo(params[:id])
     @title = memo[:title]
     @memo = memo[:memo]
     erb :show
   end
 
   get '/memos/:id/edit' do
-    memos = read_memo(params[:id])
+    memos = App.read_memo(params[:id])
     @title = memos[:title]
     @memo = memos[:memo]
     erb :edit
@@ -78,13 +79,13 @@ class App < Sinatra::Application
   patch '/memos/:id' do
     title = params[:title]
     memo = params[:memo]
-    patch_memo(params[:id], title, memo)
+    App.patch_memo(params[:id], title, memo)
 
     redirect "/memos/#{params[:id]}"
   end
 
   delete '/memos/:id' do
-    delete_memo(params[:id])
+    App.delete_memo(params[:id])
 
     redirect '/memos'
   end
